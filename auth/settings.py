8bcc2cd0a1a73b2421c620720ef6b94e9a05a63b
burzychs <burzychs@gmail.com>
2019-01-17 20:38:20 +0100

    registration

--- auth/settings.py
+++ auth/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
 
     # own
     'api',
@@ -129,3 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SITE_ID = 1
